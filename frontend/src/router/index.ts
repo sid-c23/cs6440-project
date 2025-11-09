1f87f83a78c9b619dc52e9f2be431251b0cac9a9
kaylaperry <kperry45@gatech.edu>
2025-11-09 15:36:50 -0800

    Fixed bad router config

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 //import HomeView from '../views/HomeView.vue'
+import UsersView from '../views/UsersView.vue'
+import DashboardView from '../views/DashboardView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
