ed10c742943d695f6a530172c0e47c4de1c40e3b
Sid C <schaganti@gatech.edu>
2025-11-09 14:33:18 -0800

    Remove primevue, update api server to use the devServer and switch to /api routes

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,23 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import PrimeVue from 'primevue/config'
-import Aura from '@primeuix/themes/aura'
-
 import './style.css'
 
 const app = createApp(App)
 
 app.use(router)
-app.use(PrimeVue, {
-  theme: {
-    preset: Aura,
-    options: {
-      prefix: 'p',
-      darkModeSelector: false || 'none',
-      cssLayer: false
-    }
-  },
-})
 
 app.mount('#app')
