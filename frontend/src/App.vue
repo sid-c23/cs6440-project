26510cd975e10dad7743ce8998a3442a4baf212e
Sid C <schaganti@gatech.edu>
2025-11-08 17:13:33 -0800

    Switched to tailwind css

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,14 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router';
 import HelloWorld from './components/HelloWorld.vue';
-import Menubar from 'primevue/menubar';
 import { ref, onMounted } from 'vue';
 import axios from 'axios';
 
 const users = ref([]);
 
 onMounted(async () => {
-  console.log("hi there")
   const res = await axios.get("http://localhost:8000/users");
   users.value = res.data
-  console.log(res);
 })
 
 </script>
@@ -30,4 +27,5 @@ onMounted(async () => {
   <RouterView />
 </template>
 
-<style scoped></style>
+<style scoped>
+</style>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,16 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import PrimeVue from 'primevue/config'
-import Aura from '@primeuix/themes/aura'
 
 const app = createApp(App)
 
 app.use(router)
-app.use(PrimeVue, {
-  theme: {
-    preset: Aura,
-  },
-})
 
 app.mount('#app')
